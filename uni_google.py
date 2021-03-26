11eec33f7161867232f90ed5a2f29a34e32dcdd1
bkmd11 <bkendall11@gmail.com>
2021-03-26 14:29:00 -0400

    cleaned code

--- uni_google.py
+++ uni_google.py
@@ -2,4 +2,4 @@ import webbrowser
 
 
 def uni_launch(word):
-    url = webbrowser.open(f'https://www.google.com/search?q={word} unicorn&hl=en&tbm=isch')
+    webbrowser.open(f'https://www.google.com/search?q={word} unicorn&hl=en&tbm=isch')
