57686bdb82316127242444d6089343719607cc17
bkmd11 <bkendall11@gmail.com>
2021-03-26 13:18:06 -0400

    added random

--- py_unicorn.py
+++ py_unicorn.py
@@ -8,14 +8,11 @@ from unicorns import *
 
 def main():
     parser = argparse.ArgumentParser(description='Displays unicorns if you are feeling down, or just like unicorns')
-    parser.add_argument('-t', '--text', action='store_true', help='shows a text based image in the shell')
+    parser.add_argument('-t', '--text', action='store_true', help='shows a text based image of a unicorn')
     parser.add_argument('-i', '--image', action='store_true', help='opens an image of a unicorn')
+    parser.add_argument('-r', '--random', action='store_true', help='randomly shows some time of unicorn')
 
     args = parser.parse_args()
-
-    # if args.command is None:
-    #     parser.parse_args(['-h'])
-    #     sys.exit()
 
     unicorn_list = [fat_unicorn, full_unicorn, basic_unicorn, chubby_unicorn, unicorn_head]
     pic_list = uni_image.load_images_from_folder("res/")
@@ -27,6 +24,13 @@ def main():
         uni_im = random.choice(pic_list)
         uni_im.show()
 
+    elif args.random:
+        random.choice([unicorn_list, pic_list])
+        if unicorn_list:
+            print(random.choice(unicorn_list))
+        else:
+            uni_im = random.choice(pic_list)
+            uni_im.show()
     else:
         parser.parse_args(['-h'])
         sys.exit()
