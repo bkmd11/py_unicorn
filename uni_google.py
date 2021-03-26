5850bd97b229a43eccf65faa27c91c62ac053d5a
bkmd11 <bkendall11@gmail.com>
2021-03-26 14:39:06 -0400

    added bad  docstring

--- uni_google.py
+++ uni_google.py
@@ -2,4 +2,5 @@ import webbrowser
 
 
 def uni_launch(word):
+    """I launch a google search with a givne word for unicorns"""
     webbrowser.open(f'https://www.google.com/search?q={word} unicorn&hl=en&tbm=isch')
