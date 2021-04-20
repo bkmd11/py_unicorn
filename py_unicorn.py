e0c1728e66348bf9c97eb45511385c33a98158ef
bkmd11 <bkendall11@gmail.com>
2021-04-20 16:54:57 -0400

    adding keyword for specific unicorns

--- py_unicorn.py
+++ py_unicorn.py
@@ -7,28 +7,39 @@ from unicorns.uni_corns import *
 
 
 def main():
+    ### TODO: inegrate image key word
     parser = argparse.ArgumentParser(description='Displays unicorns if you are feeling down, or just like unicorns')
     parser.add_argument('-t', '--text', action='store_true', help='shows a text based image of a unicorn')
     parser.add_argument('-i', '--image', action='store_true', help='opens an image of a unicorn')
     parser.add_argument('-r', '--random', action='store_true', help='randomly shows some time of unicorn')
-    parser.add_argument('-g', '--google', metavar='description of unicorn', nargs='?',
-                        help='googles "{description} unicorn"')
+    parser.add_argument('-g', '--google', action='store_true', help='googles "{description} unicorn"')
+    parser.add_argument('-kw', '--keyword', metavar='key word for specific unicorn', nargs='?',
+                        help='takes a key word to find a specific unicorn. used with  -i and -g')
 
     args = parser.parse_args()
 
     unicorn_list = [fat_unicorn, full_unicorn, basic_unicorn, chubby_unicorn, unicorn_head]
-    pic_list = uni_image.load_images_from_folder("res/")
+    pic_list = uni_image.load_images_from_folder()
     google_list = ['fat', 'chubby', 'majestic', 'prancing', 'rude', 'sexy', 'ripped', 'swole', 'cat', 'salty']
     random_list = [unicorn_list, pic_list, google_list]
 
     if args.text:
         print(random.choice(unicorn_list))
     elif args.image:
-        uni_im = random.choice(pic_list)
-        uni_im.show()
+        if args.keyword:
+            try:
+                uni_image.load_keyword_image(args.keyword).show()
+            except AttributeError:
+                print(uni_image.load_keyword_image(args.keyword))
+        else:
+            uni_im = random.choice(pic_list)
+            uni_im.show()
     elif args.google:
-        word = args.google
-        uni_google.uni_launch(word)
+        if args.keyword:
+            uni_google.uni_launch(args.keyword)
+        else:
+            word = random.choice(google_list)
+            uni_google.uni_launch(word)
     elif args.random:
         choice = random.choice(random_list)
 
--- unicorns/uni_image.py
+++ unicorns/uni_image.py
@@ -1,12 +1,29 @@
 import os
 from PIL import Image
 
+FOLDER = 'unicorns/res/'
+# TODO: probably should use pathlib for this
 
-def load_images_from_folder(folder):
+
+def load_images_from_folder():
     """ I load all the images in the res folder"""
     images = []
-    for filename in os.listdir(folder):
-        img = Image.open(os.path.join(folder, filename))
+    for filename in os.listdir(FOLDER):
+        img = Image.open(os.path.join(FOLDER, filename))
         if img is not None:
             images.append(img)
     return images
+
+
+def load_keyword_image(word):
+    """I attempt to find an image with a key word"""
+    jpg = '{}_uni.jpg'.format(word)
+    png = '{}_uni.png'.format(word)
+
+    files = os.listdir(FOLDER)
+    if jpg in files:
+        return Image.open(os.path.join(FOLDER, jpg))
+    elif png in files:
+        return Image.open(os.path.join(FOLDER, png))
+    else:
+        return 'Unicorn not found'
